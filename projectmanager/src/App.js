751d35e6b7bb21a4f0eb3ee2eea701d1890ac15e
Rob Waller <r.t.waller@gmail.com>
2018-03-06 13:48:56 -0600

    Finished to 37 minutes

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        title: 'Business Website',
+        category: 'Web Design'
+      },
+      {
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        title: 'E-commerce shopping cart',
+        category: 'Web Development'
+      }
+    ]});
+  }
+
+  handleAddProject(project){
+    console.log(project);
+  }
+
   render() {
     return (
       <div className="App">
-        My App
-        <Projects />
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
